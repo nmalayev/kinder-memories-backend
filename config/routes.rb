38e41a9c126509b0a9364c687aeb3404e356c47f
Nison Malayev <nmalayev@gmail.com>
2019-03-13 17:59:36 -0400

    fixed devise mistake

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :posts, only: [:index, :update, :create, :show]
-      resources :users, only: [:index, :show]
+    namespace :api do
+      namespace :v1 do
+        resources :posts, only: [:index, :update, :create, :show]
+        resources :users, only: [:index, :show]
+      end
     end
   end
-end
 end
