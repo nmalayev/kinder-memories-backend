b78fd73e73fcb9a009f285b752f5fd0bb468cfcb
Nison Malayev <nmalayev@gmail.com>
2019-03-18 10:28:02 -0400

    added memory date presence validation to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   # Only permit post_type attribute to have the following three values:
   # validates :post_type, inclusion: { in: %w(photo video letter)}
-
+  validates :memory_date, presence: true
   belongs_to :timeline
   belongs_to :user
 end
