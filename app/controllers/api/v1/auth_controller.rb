c22a14c6c9ae2432944c14f5925b6eece80254d5
Nison Malayev <nmalayev@gmail.com>
2019-03-21 10:11:33 -0400

    fixed auth controller create method = forgot @ in front of user var

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::AuthController < ApplicationController
   def create
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
-      jwt = JWT.encode({user_id: user.id}, 'super_secret')
+      jwt = JWT.encode({user_id: @user.id}, 'super_secret')
       render json: {user: @user, jwt: jwt}
     else
       render json: {errors: "Incorrect credentials. Please try again."}
