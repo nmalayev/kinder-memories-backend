2084d4b6201acb246b75af721582a59268aa6455
Nison Malayev <nmalayev@gmail.com>
2019-04-04 15:22:41 -0400

    added more data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,14 +35,6 @@ t1 = Timeline.create(name: 'Eliana',birthday: DateTime.new(2018,4,13,18,44,0,'-4
 
 # Creating posts
 
-# User.all.each do |u|
-#   post = Post.create(post_type: 'photo', title: 'first tooth', description: 'first tooth', timeline: t1, user: u, photo: '', memory_date: DateTime.new(2018,6,15))
-#   post.file.attach(io: File.open('/Users/nison/Documents/a new view of our starry night.jpg'), filename: 'pic.jpg')
-
-#   post2 = Post.create(post_type: 'video', title: 'first time feeding papa', description: 'first time you fed your dad with a spoon', timeline: t1, user: u, video: '', memory_date: DateTime.new(2018,7,15))
-#   post2.file.attach(io: File.open('/Users/nison/Desktop/baby.mp4'), filename: 'cats.mp4')
-# end
-
 # Creating letters 
 
 Post.create(post_type: 'letter', title: 'Crib Time!', description: 'First time sleeping in your crib!', timeline: t1, user: u9, photo: '', memory_date: DateTime.new(2018,8,9), letter: 'Hi Eliana! Today we came to visit you and watched you try sleeping in your big girl crib for the first time! You looked so small in that big crib, but that didn’t bother you at all! You are so brave and we love you so much! Love, Grandma
@@ -64,35 +56,71 @@ Post.create(post_type: 'letter', title: "First Valentine's Day!", description: "
 p1 = Post.create(post_type: 'photo', title: '1 Month', description: "You're 1 month old!", timeline: t1, user: u1, memory_date: DateTime.new(2018,5,13))
 p1.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/JPEGS/IMG_3485.jpg'), filename: 'pic.jpg')
 
-p1 = Post.create(post_type: 'photo', title: '2 Months', description: "You're 2 months old!", timeline: t1, user: u1, memory_date: DateTime.new(2018,6,13))
-p1.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/JPEGS/IMG_3885.jpg'), filename: 'pic.jpg')
+p2 = Post.create(post_type: 'photo', title: '2 Months', description: "You're 2 months old!", timeline: t1, user: u1, memory_date: DateTime.new(2018,6,13))
+p2.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/JPEGS/IMG_3885.jpg'), filename: 'pic.jpg')
 
-p1 = Post.create(post_type: 'photo', title: '3 Months', description: "You're 3 months old!", timeline: t1, user: u1, memory_date: DateTime.new(2018,7,13))
-p1.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/3mo.jpg'), filename: 'pic.jpg')
+p3 = Post.create(post_type: 'photo', title: '3 Months', description: "You're 3 months old!", timeline: t1, user: u1, memory_date: DateTime.new(2018,7,13))
+p3.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/3mo.jpg'), filename: 'pic.jpg')
 
-p1 = Post.create(post_type: 'photo', title: '4 Months', description: "You're 4 months old!", timeline: t1, user: u1, memory_date: DateTime.new(2018,8,13))
-p1.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/4mo.jpg'), filename: 'pic.jpg')
+p4 = Post.create(post_type: 'photo', title: '4 Months', description: "You're 4 months old!", timeline: t1, user: u1, memory_date: DateTime.new(2018,8,13))
+p4.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/4mo.jpg'), filename: 'pic.jpg')
 
-p1 = Post.create(post_type: 'photo', title: '5 Months', description: "You're 5 months old!", timeline: t1, user: u2, memory_date: DateTime.new(2018,9,13))
-p1.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/5mo.jpg'), filename: 'pic.jpg')
+p5 = Post.create(post_type: 'photo', title: '5 Months', description: "You're 5 months old!", timeline: t1, user: u2, memory_date: DateTime.new(2018,9,13))
+p5.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/5mo.jpg'), filename: 'pic.jpg')
 
-p1 = Post.create(post_type: 'photo', title: '6 Months', description: "You're 6 months old!", timeline: t1, user: u2, memory_date: DateTime.new(2018,10,13))
-p1.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/6mo.jpg'), filename: 'pic.jpg')
+p6 = Post.create(post_type: 'photo', title: '6 Months', description: "You're 6 months old!", timeline: t1, user: u2, memory_date: DateTime.new(2018,10,13))
+p6.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/6mo.jpg'), filename: 'pic.jpg')
 
-p1 = Post.create(post_type: 'photo', title: '7 Months', description: "You're 7 months old!", timeline: t1, user: u2, memory_date: DateTime.new(2018,11,13))
-p1.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/7mo.jpg'), filename: 'pic.jpg')
+p7 = Post.create(post_type: 'photo', title: '7 Months', description: "You're 7 months old!", timeline: t1, user: u2, memory_date: DateTime.new(2018,11,13))
+p7.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/7mo.jpg'), filename: 'pic.jpg')
 
-p1 = Post.create(post_type: 'photo', title: '8 Months', description: "You're 8 months old!", timeline: t1, user: u2, memory_date: DateTime.new(2018,12,13))
-p1.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/JPEGS/8mo.jpg'), filename: 'pic.jpg')
+p8 = Post.create(post_type: 'photo', title: '8 Months', description: "You're 8 months old!", timeline: t1, user: u2, memory_date: DateTime.new(2018,12,13))
+p8.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/JPEGS/8mo.jpg'), filename: 'pic.jpg')
 
-p1 = Post.create(post_type: 'photo', title: '9 Months', description: "You're 9 months old!", timeline: t1, user: u2, memory_date: DateTime.new(2019,1,13))
-p1.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/9mo.jpg'), filename: 'pic.jpg')
+p9 = Post.create(post_type: 'photo', title: '9 Months', description: "You're 9 months old!", timeline: t1, user: u2, memory_date: DateTime.new(2019,1,13))
+p9.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/9mo.jpg'), filename: 'pic.jpg')
 
-p1 = Post.create(post_type: 'photo', title: '10 Months', description: "You're 10 months old!", timeline: t1, user: u2, memory_date: DateTime.new(2019,2,13))
-p1.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/10mo.jpg'), filename: 'pic.jpg')
+p10 = Post.create(post_type: 'photo', title: '10 Months', description: "You're 10 months old!", timeline: t1, user: u2, memory_date: DateTime.new(2019,2,13))
+p10.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/10mo.jpg'), filename: 'pic.jpg')
 
-p1 = Post.create(post_type: 'photo', title: '11 Months', description: "You're 11 months old!", timeline: t1, user: u1, memory_date: DateTime.new(2019,3,13))
-p1.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/11mo.jpg'), filename: 'pic.jpg')
+p11 = Post.create(post_type: 'photo', title: '11 Months', description: "You're 11 months old!", timeline: t1, user: u1, memory_date: DateTime.new(2019,3,13))
+p11.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/11mo.jpg'), filename: 'pic.jpg')
+
+p12 = Post.create(post_type: 'video', title: 'Playing with Sasha', description: "Learning to pet our cat, Sasha", timeline: t1, user: u3, memory_date: DateTime.new(2018,12,21))
+p12.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/p12.mp4'), filename: 'video.mp4')
+
+p13 = Post.create(post_type: 'video', title: '9 Month Cuteness', description: "Being cute during your 9 month photoshoot", timeline: t1, user: u4, memory_date: DateTime.new(2019,1,13))
+p13.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/p13.mp4'), filename: 'video.mp4')
+
+p14 = Post.create(post_type: 'video', title: 'First Time Standing!', description: "First time you stood by yourself and held on!", timeline: t1, user: u5, memory_date: DateTime.new(2019,1,21))
+p14.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/p14.mp4'), filename: 'video.mp4')
+
+p15 = Post.create(post_type: 'video', title: "Lovin' That Pacifier!", description: "Here you are, 4 days old and loving your green pacifier", timeline: t1, user: u6, memory_date: DateTime.new(2018,4,17))
+p15.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/p15.mov'), filename: 'video.mov')
+
+p16 = Post.create(post_type: 'video', title: "Falling Asleep in Your Bassinet", description: "Looking around at the world instead of falling asleep", timeline: t1, user: u7, memory_date: DateTime.new(2018,4,28))
+p16.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/p16.mov'), filename: 'video.mov')
+
+p17 = Post.create(post_type: 'video', title: "New Toy!", description: "Your first time with your favorite squigly toy", timeline: t1, user: u8, memory_date: DateTime.new(2018,5,27))
+p17.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/p17.mov'), filename: 'video.mov')
+
+p18 = Post.create(post_type: 'video', title: "Tummy Time on the Changing Table", description: "One of the first times you had tummy time on the changing table - you liked it a lot more then than you do now", timeline: t1, user: u9, memory_date: DateTime.new(2018,6,6))
+p18.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/p18.mov'), filename: 'video.mov')
+
+p19 = Post.create(post_type: 'video', title: "Post Bath Tickles!", description: "Tickling your belly after bathtime", timeline: t1, user: u10, memory_date: DateTime.new(2018,7,20))
+p19.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/p19.mov'), filename: 'video.mov')
+
+p20 = Post.create(post_type: 'video', title: "Jump! Jump! Jump!", description: "Jumping in your jumper! You loved it so much! We put a book under your feet because you were too short.", timeline: t1, user: u11, memory_date: DateTime.new(2018,9,17))
+p20.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/p20.mov'), filename: 'video.mov')
+
+p21 = Post.create(post_type: 'video', title: "Learning To Wave", description: "Learning to wave while sitting with your favorite caterpillar", timeline: t1, user: u12, memory_date: DateTime.new(2019,1,6))
+p21.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/p21.mov'), filename: 'video.mov')
+
+p22 = Post.create(post_type: 'photo', title: 'Bunny Costume!', description: "Halloween costume tryout! You're smiling, so obviously it was comfy :)", timeline: t1, user: u4, memory_date: DateTime.new(2019,3,13))
+p22.file.attach(io: File.open('/Users/nison/Desktop/Kinder Moments/p22.jpg'), filename: 'pic.jpg')
+
+
+
 
 
 
